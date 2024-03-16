1c0dc2f36eee76dd7c04f7847c0d55995d1e1142
Asem7Yousry.he/him <asem23yousry@gmail.com>
2024-03-16 22:54:41 +0200

    link static and media files of the system

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os ### to deat with pathes in os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +122,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+### connect static files directory to the whole ui of system
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
+### url of media files 
+MEDIA_URL = "media/"
+
+## connect all media files of the system 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+### to deal with static and media files ###
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+### link all static files of system to all urls 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+### link all media files of system to all urls 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
